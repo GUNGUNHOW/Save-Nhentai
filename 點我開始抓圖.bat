f399f4acd69fe701cffe12965f270a2ee7406918
Nesl <33894721+neslxzhen@users.noreply.github.com>
2019-07-30 00:27:41 +0800

    Update 點我開始抓圖.bat

--- 點我開始抓圖.bat
+++ 點我開始抓圖.bat
@@ -1,9 +1,9 @@
 cd /d %~dp0 
-xeHentai-2.0.1.9.exe -u ##你的帳號 -k ##你的密碼 https://e-hentai.org/g/1328603/668ad55bcd/
-xeHentai-2.0.1.9.exe -u ##你的帳號 -k ##你的密碼 https://e-hentai.org/g/1326311/2b608c4c6a/
-xeHentai-2.0.1.9.exe -u ##你的帳號 -k ##你的密碼 https://e-hentai.org/g/1327346/8c385a3696/
-xeHentai-2.0.1.9.exe -u ##你的帳號 -k ##你的密碼 https://e-hentai.org/g/1327357/e319a64e2b/
-xeHentai-2.0.1.9.exe -u ##你的帳號 -k ##你的密碼 https://e-hentai.org/g/1325027/8f152378f9/
-xeHentai-2.0.1.9.exe -u ##你的帳號 -k ##你的密碼 https://e-hentai.org/g/1325629/46fbd5da8b/
-xeHentai-2.0.1.9.exe -u ##你的帳號 -k ##你的密碼 https://e-hentai.org/g/1186679/2feb83e9c4/
+xeHentai-2.0.1.9.exe https://e-hentai.org/g/1328603/668ad55bcd/
+xeHentai-2.0.1.9.exe https://e-hentai.org/g/1326311/2b608c4c6a/
+xeHentai-2.0.1.9.exe https://e-hentai.org/g/1327346/8c385a3696/
+xeHentai-2.0.1.9.exe https://e-hentai.org/g/1327357/e319a64e2b/
+xeHentai-2.0.1.9.exe https://e-hentai.org/g/1325027/8f152378f9/
+xeHentai-2.0.1.9.exe https://e-hentai.org/g/1325629/46fbd5da8b/
+xeHentai-2.0.1.9.exe https://e-hentai.org/g/1186679/2feb83e9c4/
 pause
